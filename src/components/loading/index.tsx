6d7476baa894f4e29e38aa83f341bc4fecf7933c
goesrafaela <goes.rafaelasilva@gmail.com>
2024-12-12 15:55:05 -0300

    Inicio do projeto

---
+++ src/components/loading/index.tsx
@@ -0,0 +1,10 @@
+/**Estrutura */
+
+import { ActivityIndicator } from "react-native";
+
+import {s} from "./styles";
+import { colors } from "@/styles/theme";
+
+export function Loading() {
+    return <ActivityIndicator color={colors.green.base} style={s.container}/>
+}
