edb17b598ced52a51e770c6ec07a92dc5b90483e
goesrafaela <goes.rafaelasilva@gmail.com>
2024-12-16 10:41:54 -0300

    Implementação dos components

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,13 +1,14 @@
-import {View, Text} from 'react-native'
+import { View, Text } from "react-native";
 
-export default function iIndex(){
-    return (
-        <View 
-            style={{
-                flex:1, 
-                justifyContent: 'center', 
-                alignItems:'center'}}>
-            <Text>Hello React Native</Text>
-        </View>
-    )
+/**Componentes criados */
+import { Welcome } from "@/components/welcome";
+import { Steps } from "@/components/steps";
+
+export default function Index() {
+  return (
+    <View style={{ flex: 1, padding: 40, gap: 40 }}>
+      <Welcome />
+      <Steps />
+    </View>
+  );
 }
